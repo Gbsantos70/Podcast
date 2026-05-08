e3e317b2fc75ede78b9b677c1aa707bb0cf99d37
Gbsantos70 <gabriel.s.souza8@aluno.senai.br>
2026-05-07 21:32:10 -0300

    feat: comportamento de acordeão nos cards de episódio

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ const cards = document.querySelectorAll('.card-ep');
 cards.forEach(function (card) {
     card.addEventListener('click', function () {
         const descricao = card.querySelector('.descricao-expandida')
+        cards.forEach(function (outroCard) {
+            outroCard.classList.remove('ativo');
+            outroCard.querySelector('.descricao-expandida').style.maxHeight = '0px';
+        })
         if (card.classList.toggle('ativo')) {
             descricao.style.maxHeight = '200px';
-        }         else {
+        } else {
             descricao.style.maxHeight = '0px';
         }
     });
