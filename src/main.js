bcde861df3c98bac4c6004585dcb6a18fcc3de6c
Gbsantos70 <gabriel.s.souza8@aluno.senai.br>
2026-05-11 19:25:10 -0300

    feat: expansão dos cards de integrante com acordeão

--- src/main.js
+++ src/main.js
@@ -15,3 +15,19 @@ cards.forEach(function (card) {
     });
 });
 
+const integrantes = document.querySelectorAll('.integrantes .card-integrantes');
+
+integrantes.forEach(function (integrante) {
+    integrante.addEventListener('click', function () {
+        const descricao = integrante.querySelector('.integrante-expandido')
+        integrantes.forEach(function (outroIntegrante) {
+            outroIntegrante.classList.remove('ativo');
+            outroIntegrante.querySelector('.integrante-expandido').style.maxHeight = '0px';
+        });
+        if (integrante.classList.toggle('ativo')) {
+            descricao.style.maxHeight = '200px';
+        } else {
+            descricao.style.maxHeight = '0px';
+        }
+    });
+});
