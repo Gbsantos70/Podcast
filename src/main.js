cffa4ea63780c6ebfebd3cdb9d13f4e65897d3ee
Gbsantos70 <gabriel.s.souza8@aluno.senai.br>
2026-05-21 20:49:11 -0300

    feat: página individual do Gabriel

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ const cards = document.querySelectorAll('.card-ep');
 cards.forEach(function (card) {
     card.addEventListener('click', function () {
         const descricao = card.querySelector('.descricao-expandida')
+        const estaAberto = card.classList.contains('ativo');
         cards.forEach(function (outroCard) {
             outroCard.classList.remove('ativo');
             outroCard.querySelector('.descricao-expandida').style.maxHeight = '0px';
         })
-        if (card.classList.toggle('ativo')) {
+        if (!estaAberto) {
+            card.classList.add('ativo');
             descricao.style.maxHeight = '200px';
         } else {
             descricao.style.maxHeight = '0px';
@@ -20,14 +22,17 @@ const integrantes = document.querySelectorAll('.integrantes .card-integrantes');
 integrantes.forEach(function (integrante) {
     integrante.addEventListener('click', function () {
         const descricao = integrante.querySelector('.integrante-expandido')
+        const estaAberto = integrante.classList.contains('ativo');
         integrantes.forEach(function (outroIntegrante) {
             outroIntegrante.classList.remove('ativo');
             outroIntegrante.querySelector('.integrante-expandido').style.maxHeight = '0px';
         });
-        if (integrante.classList.toggle('ativo')) {
+        if (!estaAberto) {
+            integrante.classList.add('ativo');
             descricao.style.maxHeight = '200px';
         } else {
             descricao.style.maxHeight = '0px';
         }
     });
 });
+
